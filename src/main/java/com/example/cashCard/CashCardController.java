8900835cb3c7afd98aa598d497ad156a60a0c9f7
efe okyar <f.efe.okyar@gmail.com>
2024-10-11 20:33:52 +0300

    implemented POST

--- src/main/java/com/example/cashCard/CashCardController.java
+++ src/main/java/com/example/cashCard/CashCardController.java
@@ -1,10 +1,10 @@
 package com.example.cashCard;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
@@ -27,5 +27,12 @@ class CashCardController {
             return  ResponseEntity.notFound().build();
         }
 
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest,UriComponentsBuilder ucb){
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb.path("cashcards/{id}").buildAndExpand(savedCashCard.id()).toUri();
+        return  ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
